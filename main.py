81326c71cc6de431895fc74dbfb4e0bb83b866ab
Shrinidhi Thirumalai <shrinidhit@Shrinidhi.(none)>
2014-10-23 02:44:28 -0400

    Organizing Create_World()

--- main.py
+++ main.py
@@ -54,13 +54,45 @@ def biconnect (fr,dir,to):
     connect(fr,dir,to)
     connect(to,REVERSE[dir],fr)
 
-#Creating World
-def create_world ():
-    #Connecting Locations
+def static_world():
+    mh353 = Room('Riccardo Office','Where Riccardo works.')
+    mh3rd = Room('Milas Hall Third Floor','3rd Floor of Milas Hall')
+    mh2nd = Room('Milas Hall Second Floor','2nd Floor of Milas Hall')
+    mh1st = Room('Milas Hall First Floor','1st Floor of Milas Hall')
+    oval = Room('Oval','Quite round in appearance.')
+    ac1st = Room('Academic Center First Floor','Ah, the sweet smell of knowledge.')
+    ac113 = Room('Academic Center 113','Home of GPro')
+    cc1st = Room('Campus Center First Floor','Dat food though.')
+    wh1 = Room('West Hall First Floor','1st Floor of West Hall')
+    wh2 = Room('West Hall Second Floor','2nd Floor of West Hall')
+    wh3 = Room('West Hall Third Floor','3rd Floor of West Hall')
+    wh4 = Room('West Hall Fourth Floor','4th Floor of West Hall')
+    eh1 = Room('East Hall First Floor','1st Floor of East Hall')
+    eh2 = Room('East Hall Second Floor','2nd Floor of East Hall')
+    eh3 = Room('East Hall Third Floor','3rd Floor of East Hall')
+    eh4 = Room('East Hall Fourth Floor','4th Floor of East Hall')
+    babson = Room('Babson College','BABBIES')
+
+    Connections = [[mh353, 'east',  mh3rd],
+    [mh3rd, 'down',  mh2nd],
+    [mh2nd, 'down',  mh1st],
+    [mh1st, 'north',  oval],
+    [oval, 'east',  cc1st],
+    [cc1st, 'east',  wh1],
+    [wh1, 'up', wh2],
+    [wh2, 'up', wh3],
+    [wh3, 'up', wh4],
+    [wh1, 'east', eh1],
+    [eh1, 'up', eh2],
+    [eh2, 'up', eh3],
+    [eh3, 'up', eh4],
+    [oval, 'north',  babson],
+    [oval, 'west',  ac1st],
+    [ac1st, 'north',  ac113]]
+
     for con in Connections:
         biconnect(con[0], con[1], con[2])
 
-    #Player Creation: The player is the first 'thing' that has to be created
     Player('Blubbering-Fool', oval, "That's you!")
 
     #Needed Objects:
@@ -79,7 +111,10 @@ def create_world ():
     Computer('hal-9000', ac113,'Knows too much...suspiciously not human')
     Computer('johnny-5', eh1,'Kind of adorable. A little monotoned and clinky but hey.')
     Professor('Riccardo',mh353,"He's the best!",random.randint(1,5), random.randint(1,5))
-    
+
+#Creating World
+def create_world ():
+    static_world()    
     #Random Choosings:
     for homework in homeworks:
         Homework(homework,
@@ -106,8 +141,7 @@ def create_world ():
         Butterfly(random.choice(ButterflyR.names), random.choice(Room.rooms), random.choice(ButterflyR.messages))
 
     for i in range(random.randint(2,4)):
-        TrollHunter(random.choice(TrollHunterR.names),mh1st, random.choice(TrollHunterR.messages) ,random.randint(1,3),random.randint(1,8))
-
+        TrollHunter(random.choice(TrollHunterR.names), random.choice(Room.rooms), random.choice(TrollHunterR.messages) ,random.randint(1,3),random.randint(1,8))
 
 def print_tick_action (t):
     Player.me.location().report('The clock ticks '+str(t))
